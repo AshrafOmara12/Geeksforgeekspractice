b6625d4203326ce496aa1b01e9eb5fddd089d73d
unknown <ashrafomara33@gmail.com>
2022-09-28 22:16:06 +0200

    adding 4 methods for removing the empty lists from a list

---
+++ emptylist.py
@@ -0,0 +1,30 @@
+test_list = [5, 6, [], 3, [], [], 9]
+
+# Method 1
+
+l = []
+
+for i in test_list:
+    if i == []:
+        continue
+    else:
+        l.append(i)
+
+# print(l)
+
+# method 2
+
+l1 = [i for i in test_list if i !=[] ]
+# print(x)
+
+# method 3
+
+l2 = list(filter(None,test_list))
+# print(l2)
+
+# method 4
+
+while [] in test_list:
+    test_list.remove([])
+
+print(test_list)
