8d96bbb805a88fe485cf17514e5a40b9fa834ae7
unknown <ashrafomara33@gmail.com>
2022-09-30 18:50:30 +0200

    scraping string functions

--- scrapingtasks/bs4scarpe1.py
+++ scrapingtasks/bs4scarpe1.py
@@ -1,19 +1,42 @@
-import bs4 as bs
+from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+
 
 url = 'https://www.geeksforgeeks.org/python-list/'
+# url = 'https://www.geeksforgeeks.org/transparent-window-in-tkinter/'
 url_link = requests.get(url)
 
-# print(url_link.text)
-file = bs.BeautifulSoup(url_link.text,'lxml')
-# print(f
-find_table = file.find("figure", {"class": "table"})
+file = BeautifulSoup(url_link.content,'html.parser')
 
-description = []
-functions = []
+table = file.find('figure', class_='table')
+# header = table.find('tr')
+
+# headers = []
+#
+# for row in header:
+#     headers.append(row.text)
+
+table_data = table.findAll('td')
+Function_Name = []
+Description = []
 links = []
+for td in table_data:
+    if td.a != None:
+        Function_Name.append(td.a.text)
+        links.append(td.a['href'].strip())
+    else:
 
-for td in find_table.find_all('td'):
-    print(td.a.text)
+        Description.append(td.text)
 
+# df = pd.DataFrame(links)
+# print(df)
+# columns = ['Function Name', 'Function Description','Links']
+# d
+# print(df_1)
+data_dict = {'Function Name':Function_Name,'Function Description':Description,
+             'Links':links}
 
+df = pd.DataFrame(data_dict)
+
+df.to_csv('final', index=False)
